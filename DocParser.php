5edc1324bc9a1e66540701c745ab6cf0a0a276cf
zhaoyj <admin@yjwmidc.com>
2026-01-04 17:31:20 +0800

    缓存

--- DocParser.php
+++ DocParser.php
@@ -1,104 +1,202 @@
 <?php
 namespace app\admin\controller\doc;
 
+use think\facade\Cache;
+
 class DocParser
 {
-    private $params = array ();
+    private $params = array();
+    
+    /**
+     * 缓存过期时间（秒）
+     */
+    private $cacheExpire = 7200; // 2小时
+    
+    /**
+     * 缓存标签
+     */
+    private $cacheTag = 'apidoc_parser';
+    
+    /**
+     * 是否启用缓存
+     */
+    private $enableCache = true;
 
     /**
      * 解析注释
      * @param string $doc
+     * @param string $cacheKey 可选的缓存键，用于标识不同的文档
      * @return array
      */
-    public function parse($doc = '') {
+    public function parse($doc = '', $cacheKey = '') {
         if ($doc == '') {
             return $this->params;
         }
+        
+        // 如果启用缓存且提供了缓存键
+        if ($this->enableCache && !empty($cacheKey)) {
+            $fullCacheKey = 'doc_parse_' . md5($cacheKey . $doc);
+            
+            // 尝试从缓存获取
+            $cachedResult = Cache::get($fullCacheKey);
+            if ($cachedResult !== false && $cachedResult !== null) {
+                return $cachedResult;
+            }
+        }
+        
+        // 重置参数
+        $this->params = array();
+        
         // Get the comment
-        if (preg_match ( '#^/\*\*(.*)\*/#s', $doc, $comment ) === false)
+        if (preg_match('#^/\*\*(.*)\*/#s', $doc, $comment) === false) {
             return $this->params;
-        $comment = trim ( $comment [1] );
+        }
+        
+        $comment = trim($comment[1]);
+        
         // Get all the lines and strip the * from the first character
-        if (preg_match_all ( '#^\s*\*(.*)#m', $comment, $lines ) === false)
+        if (preg_match_all('#^\s*\*(.*)#m', $comment, $lines) === false) {
             return $this->params;
-        $this->parseLines ( $lines [1] );
+        }
+        
+        $this->parseLines($lines[1]);
+        
+        // 如果启用缓存且提供了缓存键，保存到缓存
+        if ($this->enableCache && !empty($cacheKey) && !empty($this->params)) {
+            Cache::tag($this->cacheTag)->set($fullCacheKey, $this->params, $this->cacheExpire);
+        }
+        
         return $this->params;
+    }
+    
+    /**
+     * 设置缓存过期时间
+     * @param int $seconds
+     * @return $this
+     */
+    public function setCacheExpire($seconds) {
+        $this->cacheExpire = $seconds;
+        return $this;
+    }
+    
+    /**
+     * 启用或禁用缓存
+     * @param bool $enable
+     * @return $this
+     */
+    public function setEnableCache($enable) {
+        $this->enableCache = $enable;
+        return $this;
+    }
+    
+    /**
+     * 清除解析器缓存
+     * @return bool
+     */
+    public function clearCache() {
+        try {
+            Cache::tag($this->cacheTag)->clear();
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+    
+    /**
+     * 清除特定文档的缓存
+     * @param string $cacheKey
+     * @param string $doc
+     * @return bool
+     */
+    public function clearDocCache($cacheKey, $doc) {
+        if (empty($cacheKey) || empty($doc)) {
+            return false;
+        }
+        
+        $fullCacheKey = 'doc_parse_' . md5($cacheKey . $doc);
+        return Cache::delete($fullCacheKey);
     }
     
     private function parseLines($lines) {
         $desc = [];
-        foreach ( $lines as $line ) {
-            $parsedLine = $this->parseLine ( $line ); // Parse the line
+        foreach ($lines as $line) {
+            $parsedLine = $this->parseLine($line); // Parse the line
 
-            if ($parsedLine === false && ! isset ( $this->params ['description'] )) {
-                if (isset ( $desc )) {
+            if ($parsedLine === false && !isset($this->params['description'])) {
+                if (isset($desc)) {
                     // Store the first line in the short description
-                    $this->params ['description'] = implode ( PHP_EOL, $desc );
+                    $this->params['description'] = implode(PHP_EOL, $desc);
                 }
-                $desc = array ();
+                $desc = array();
             } elseif ($parsedLine !== false) {
-                $desc [] = $parsedLine; // Store the line in the long description
+                $desc[] = $parsedLine; // Store the line in the long description
             }
         }
-        $desc = implode ( ' ', $desc );
-        if (! empty ( $desc ))
-            $this->params ['long_description'] = $desc;
+        $desc = implode(' ', $desc);
+        if (!empty($desc)) {
+            $this->params['long_description'] = $desc;
+        }
     }
     
     private function parseLine($line) {
         // trim the whitespace from the line
-        $line = trim ( $line );
+        $line = trim($line);
 
-        if (empty ( $line ))
+        if (empty($line)) {
             return false; // Empty line
+        }
 
-        if (strpos ( $line, '@' ) === 0) {
-            if (strpos ( $line, ' ' ) > 0) {
+        if (strpos($line, '@') === 0) {
+            if (strpos($line, ' ') > 0) {
                 // Get the parameter name
-                $param = substr ( $line, 1, strpos ( $line, ' ' ) - 1 );
-                $value = substr ( $line, strlen ( $param ) + 2 ); // Get the value
+                $param = substr($line, 1, strpos($line, ' ') - 1);
+                $value = substr($line, strlen($param) + 2); // Get the value
             } else {
-                $param = substr ( $line, 1 );
+                $param = substr($line, 1);
                 $value = '';
             }
             // Parse the line and return false if the parameter is valid
-            if ($this->setParam ( $param, $value ))
+            if ($this->setParam($param, $value)) {
                 return false;
+            }
         }
 
         return $line;
     }
     
     private function setParam($param, $value) {
-        if ($param == 'param' || $param == 'header')
-            $value = $this->formatParam( $value );
-        if ($param == 'class')
-            list ( $param, $value ) = $this->formatClass ( $value );
+        if ($param == 'param' || $param == 'header') {
+            $value = $this->formatParam($value);
+        }
+        if ($param == 'class') {
+            list($param, $value) = $this->formatClass($value);
+        }
 
-        if($param == 'return' || $param == 'param' || $param == 'header'){
-            $this->params [$param][] = $value;
-        }else if (empty ( $this->params [$param] )) {
-            $this->params [$param] = $value;
+        if ($param == 'return' || $param == 'param' || $param == 'header') {
+            $this->params[$param][] = $value;
+        } else if (empty($this->params[$param])) {
+            $this->params[$param] = $value;
         } else {
-            $this->params [$param] = $this->params [$param] . $value;
+            $this->params[$param] = $this->params[$param] . $value;
         }
         return true;
     }
     
     private function formatClass($value) {
-        $r = preg_split ( "[\(|\)]", $value );
-        if (is_array ( $r )) {
-            $param = $r [0];
-            parse_str ( $r [1], $value );
-            foreach ( $value as $key => $val ) {
-                $val = explode ( ',', $val );
-                if (count ( $val ) > 1)
-                    $value [$key] = $val;
+        $r = preg_split("[\(|\)]", $value);
+        if (is_array($r)) {
+            $param = $r[0];
+            parse_str($r[1], $value);
+            foreach ($value as $key => $val) {
+                $val = explode(',', $val);
+                if (count($val) > 1) {
+                    $value[$key] = $val;
+                }
             }
         } else {
             $param = 'Unknown';
         }
-        return array (
+        return array(
             $param,
             $value
         );
@@ -153,8 +251,7 @@ class DocParser
         return [];
     }
 
-
-    private function getParamType($type){
+    private function getParamType($type) {
         $typeMaps = [
             'string' => '字符串',
             'int' => '整型',
@@ -166,6 +263,31 @@ class DocParser
             'enum' => '枚举类型',
             'object' => '对象',
         ];
-        return array_key_exists($type,$typeMaps) ? $typeMaps[$type] : $type;
+        return array_key_exists($type, $typeMaps) ? $typeMaps[$type] : $type;
+    }
+    
+    /**
+     * 批量解析多个文档
+     * @param array $docs 文档数组 ['key' => 'doc_content']
+     * @return array
+     */
+    public function batchParse($docs) {
+        $results = [];
+        foreach ($docs as $key => $doc) {
+            $results[$key] = $this->parse($doc, $key);
+        }
+        return $results;
+    }
+    
+    /**
+     * 获取缓存统计信息
+     * @return array
+     */
+    public function getCacheStats() {
+        return [
+            'cache_enabled' => $this->enableCache,
+            'cache_expire' => $this->cacheExpire,
+            'cache_tag' => $this->cacheTag,
+        ];
     }
 }
